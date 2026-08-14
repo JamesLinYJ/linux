89823c627c875bd18864f5dc377515a289472d15
ESP32-S3 Linux project <noreply@example.invalid>
2026-08-14 11:51:10 +0800

    dt-bindings: clock: add ESP32-S3 GDMA IDs

--- include/dt-bindings/reset/esp32s3.h
+++ include/dt-bindings/reset/esp32s3.h
@@ -5,7 +5,8 @@
 #define ESP32S3_RST_I2C0	0
 #define ESP32S3_RST_I2C1	1
 #define ESP32S3_RST_SPI3	2
+#define ESP32S3_RST_GDMA	3
 
-#define ESP32S3_RST_NUM		3
+#define ESP32S3_RST_NUM		4
 
 #endif /* _DT_BINDINGS_RESET_ESP32S3_H */
