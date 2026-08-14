7ab0aa1ddf3b5f2368c5d3cf48e52808ca744abc
ESP32-S3 Linux project <noreply@example.invalid>
2026-08-14 11:12:24 +0800

    dt-bindings: clock: add ESP32-S3 system controller

---
+++ include/dt-bindings/clock/esp32s3.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+#ifndef _DT_BINDINGS_CLOCK_ESP32S3_H
+#define _DT_BINDINGS_CLOCK_ESP32S3_H
+
+#define ESP32S3_CLK_I2C0	0
+#define ESP32S3_CLK_I2C1	1
+
+#define ESP32S3_CLK_NUM		2
+
+#endif /* _DT_BINDINGS_CLOCK_ESP32S3_H */
---
+++ include/dt-bindings/reset/esp32s3.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+#ifndef _DT_BINDINGS_RESET_ESP32S3_H
+#define _DT_BINDINGS_RESET_ESP32S3_H
+
+#define ESP32S3_RST_I2C0	0
+#define ESP32S3_RST_I2C1	1
+
+#define ESP32S3_RST_NUM		2
+
+#endif /* _DT_BINDINGS_RESET_ESP32S3_H */
