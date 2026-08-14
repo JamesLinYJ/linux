825c172d5b95f0f40903a6b9196bbf56c39308f5
ESP32-S3 Linux project <noreply@example.invalid>
2026-08-14 11:16:29 +0800

    license: use GPL-2.0-only for ESP32-S3 support

--- include/dt-bindings/reset/esp32s3.h
+++ include/dt-bindings/reset/esp32s3.h
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/* SPDX-License-Identifier: GPL-2.0-only */
 #ifndef _DT_BINDINGS_RESET_ESP32S3_H
 #define _DT_BINDINGS_RESET_ESP32S3_H
 
